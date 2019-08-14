4639f7ec33404ea07ff86fb12962ce9406e08654
firas <firas@gmail.com>
2019-08-14 12:05:32 +0100

    first commit

---
+++ src/main/java/com/example/demo/SeleniumspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumspringApplication.class, args);
+	}
+
+}
